01b9bcb796d4db10d107d5d6d391064beaaaadac
stasiexm <tkacorzyk@gmail.com>
2023-10-04 17:12:10 +0200

    added script

--- TomaszKprojekt/Assets/Scripts/Level.cs
+++ TomaszKprojekt/Assets/Scripts/Level.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
-public class game : MonoBehaviour
+public class Level : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
+    void OnTriggerEnter2D(Collider2D colilision)
+    {
+        SceneManager.LoadScene("Lose");
+    }
     // Update is called once per frame
     void Update()
     {
